de8ac5aee228150507d4edb6e2cc98a2e5f17e9a
mikeill <mike@mzoo.org>
2022-08-29 12:51:49 -0500

    Don't try to sync removed js dir.

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/usr/bin
-rsync -avP stylesheet *.css *.php js ptsustain:public_html/wp-content/themes/ProsperitySustainablePopulation/
+rsync -avP stylesheet *.css *.php ptsustain:public_html/wp-content/themes/ProsperitySustainablePopulation/
